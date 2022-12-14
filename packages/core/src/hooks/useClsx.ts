cc841987a213e7910864f78212bdc4b662c935a1
janpoem <janpoem@163.com>
2022-12-14 10:05:42 +0800

    feat(useClsx): add useClsx

---
+++ packages/core/src/hooks/useClsx.ts
@@ -0,0 +1,10 @@
+import clsx from 'clsx';
+import { useMemo } from 'react';
+
+export type ClassNameType = string | null | undefined;
+
+export function useClsx(...cls: ClassNameType[]): string {
+  return useMemo(() => {
+    return clsx(...cls);
+  }, cls); // eslint-disable-line react-hooks/exhaustive-deps
+}
